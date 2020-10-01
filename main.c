45695a5bf6d3997f4da502bb45b7879185673cc6
jrpasillas <69991329+jrpasillas@users.noreply.github.com>
2020-10-01 01:34:26 -0600

    Update main.c

--- main.c
+++ main.c
@@ -12,13 +12,41 @@
 int main(void) {
   int s, d;
 
-  // Read Systolic and Diastolic pressure
+   // Read Systolic and Diastolic pressure
   scanf("%d %d", &s, &d);
-
-  // TODO: Complete the rest of the categories
-  if(s < 120) {
-    if(d < 80) {
+  // Systolic pressure must be greater than Diastolic pressure to move on, if not, 'Error' 
+  if(s < d)
+      printf("Error");
+  else {
+   // Both Systolic and Diastolic pressure must be in their range in order to be 'Normal' (AND)
+  if((s < 120) && (d < 80))
+  {
       printf("Normal\n");
-    }
   }
+  // Both Systolic and Diastolic pressure must be in their range in order to be 'Elevated' (AND)
+  if((s >= 120 && s <= 129) && (d < 80))
+  {
+      printf("Elevated\n");
+  }
+  // Systolic and Diastolic pressure must be in their own range BUT both (s) and (d) don't have to be true (OR)
+  if((s >= 130 && s <= 139) || (d >= 80 && d <= 89)) 
+  {   
+      printf("Stage 1 Hypertension\n");
+  }
+  // Systolic and Diastolic pressure must be in their own range BUT both (s) and (d) don't have to be true (OR)
+  if((s > 140) || (d > 90)) 
+  {
+      printf("Stage 2 Hypertension\n"); 
+  }
+  // Both (s) and (d) must be in their own range and either both could be true or one of them (AND/OR)
+  if((s > 180) && (d > 120)) 
+  {  
+    if((s > 180) || (d > 120))
+      printf("Hypertensive Crysis\n");
+  }
+  
+  }
+
+  return 0;
+  
 }
